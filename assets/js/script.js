061d2daa05b15eecae0bf182c42cbf69bf54a1c6
icvalle <77241566+icvalle@users.noreply.github.com>
2021-04-04 17:36:30 -0400

    Revert "api key updated."

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,6 @@
 // var lyrics = prompt("Enter lyrics. Leave blank if unknown.");
 // lyrics = lyrics.replace(" ", "%20");
 
-var apiKey = "60ad462c9bff52e81700f57b3ba8db73";
-
 var searchResultEl = document.getElementById('search-results');
 
 var artistInputEL = document.getElementById("artistInput");
@@ -16,21 +14,21 @@ function getParams() {
 
 	var singer = searchParamArr[0].split('=').pop();
 	var title = searchParamArr[1];
-	var lyrics = searchParamArr[2];
+	var lyricsQuery = searchParamArr[2];
 
 	console.log(singer);
 	console.log(title);
-	console.log(lyrics);
+	console.log(lyricsQuery);
 
-	searchMusicMatch(singer, title, lyrics);
+	searchMusicMatch(singer, title, lyricsQuery);
 
 }
 
-function searchMusicMatch(singer, title, lyrics) {
+function searchMusicMatch(singer, title, lyricsQuery) {
 	var queryUrl = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_track=";
 
-	if (singer || title || lyrics) {
-		queryUrl = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_track=" + title + "&q_artist=" + singer + "&f_has_lyrics=" + lyrics + "&quorum_factor=1&apikey=" + apiKey;
+	if (singer || title || lyricsQuery) {
+		queryUrl = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_track=" + title + "&q_artist=" + singer + "&f_has_lyrics=" + lyricsQuery + "&quorum_factor=1&apikey=4b67acae62e7e4fd972ec37a8881242b";
 
 		console.log(queryUrl);
 
@@ -40,6 +38,7 @@ function searchMusicMatch(singer, title, lyrics) {
 	}
 }
 
+// var url = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_track=" + track + "&q_artist=" + artist + "&f_has_lyrics=" + lyrics + "&quorum_factor=1&apikey=4b67acae62e7e4fd972ec37a8881242b";
 
 function artists(file) {
 	var cond = file.search("track_name");
@@ -67,11 +66,11 @@ function artists(file) {
 
 }
 
-// $('#startSearchBtn').click(function(event){
-//   fetch(url)
-//     .then(response => response.text())
-//     .then(data => (artists(data)));
-// });
+$('#startSearchBtn').click(function(event){
+  fetch(url)
+    .then(response => response.text())
+    .then(data => (artists(data)));
+});
 
     
 // var youTubeApiKey = 'AIzaSyAMgCp_vfENiw84ymkpYJyxJrT8pY1BuPI';
@@ -94,14 +93,9 @@ function searchCriteria() {
     var track = document.getElementById("title-input").value;
     var artist = document.getElementById("artist-input").value;
     var lyrics = document.getElementById("lyrics-input").value;
-	lyrics = lyrics.replace(" ", "%20");
-	
-	if (!track && !artist && !lyrics) {
-		alert("enter search criteria")
-		return;
-	}
+    lyrics = lyrics.replace(" ", "%20");
 
-    var url = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_track=" + track + "&q_artist=" + artist + "&f_has_lyrics=" + lyrics + "&quorum_factor=1&apikey=" + apiKey;
+    var url = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_track=" + track + "&q_artist=" + artist + "&f_has_lyrics=" + lyrics + "&quorum_factor=1&apikey=4b67acae62e7e4fd972ec37a8881242b";
 
     fetch(url)
 	.then(response => response.text())
@@ -110,20 +104,6 @@ function searchCriteria() {
 	console.log(artist);
 	console.log(track);
 	console.log(lyrics);
-}
-
-
-function searchLyrics() {
-
-	var lyrics = document.getElementById("lyrics-input").value;
-	lyrics = lyrics.replace(" ", "%20");
-	
-	console.log(lyrics);
-
-
-	fetch(lyricsurl)
-			.then(response => response.text())
-			.then(data => (lyric(data)));
 }
 
 	

69acca61c3856d0b7e96992bca88e9c9fcc668d7
Iris Valle <iris.valle.design@gmail.com>
2021-04-04 00:36:29 -0400

    Added error msg to index page, added styles to results using js to append class

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -2,15 +2,19 @@ var searchFormEl = document.getElementById("startSearchBtn");
 
 function searchFormSubmit(event) {
 	event.preventDefault();
-  
+	$('#searchLabel').append("");
 	var artistInputVal = document.getElementById("artistInput").value;
 	var songInputVal = document.getElementById("songInput").value;
 	var lyricsInputVal = document.getElementById("lyricsInput").value;
   
-	/*if (!artistInputVal) {
-	  console.log('You need an Artist or Song or Lyrics to start the search!');
+	if (!artistInputVal && !songInputVal && !lyricsInputVal) {
+		function alertErrorMsg() {
+			$('#searchLabel').append(' <span class="errorMsg">* You need an artist, song or lyrics to start the search!</span>');
+		}
+		alertErrorMsg();
+		setTimeout(function(){ $('#searchLabel').text("Search:"); }, 5000);
 	  return;
-	}*/
+	}
   
 	var queryString = './search-results.html?q=' + artistInputVal + '&' + songInputVal + '&' + lyricsInputVal;
 

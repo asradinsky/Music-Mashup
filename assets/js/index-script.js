bc632a5add6429f55698ac052fae39bd8f39a189
asradinsky <asradinsky@gmail.com>
2021-04-04 22:25:20 -0400

    final product?

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -5,18 +5,17 @@ function searchFormSubmit(event) {
 	$('#searchLabel').append("");
 	var artistInputVal = document.getElementById("artistInput").value;
 	var songInputVal = document.getElementById("songInput").value;
-	var lyricsInputVal = document.getElementById("lyricsInput").value;
   
 	if (!artistInputVal && !songInputVal && !lyricsInputVal) {
 		function alertErrorMsg() {
-			$('#searchLabel').append(' <span class="errorMsg">* You need an artist, song or lyrics to start the search!</span>');
+			$('#searchLabel').append(' <span class="errorMsg">* You need an artist or song to start the search!</span>');
 		}
 		alertErrorMsg();
 		setTimeout(function(){ $('#searchLabel').text("Search:"); }, 5000);
 	  return;
 	}
   
-	var queryString = './search-results.html?q=' + artistInputVal + '&' + songInputVal + '&' + lyricsInputVal;
+	var queryString = './search-results.html?q=' + artistInputVal + '&' + songInputVal;
 
 	console.log(queryString);
   
